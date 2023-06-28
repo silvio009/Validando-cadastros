a271e3dda201b147cb348708e77eb95b6522c87b
silvio009 <rm550821@fiap.com.br>
2023-06-28 14:35:25 -0300

    ativando camera

--- formulario JavaScript/JS/camera.js
+++ formulario JavaScript/JS/camera.js
@@ -3,5 +3,11 @@ const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 
 botaoIniciarCamera.addEventListener("click",async function(){
+  const iniciarVideo = await navigator.mediaDevices.
+  getUserMedia({video: true, audio: false})
 
+  botaoIniciarCamera.style.display = "none";
+  campoCamera.style.display = "block";
+
+  video.srcObject = iniciarVideo;
 })
