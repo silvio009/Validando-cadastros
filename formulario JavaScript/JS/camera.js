1a0f1e8f7f2813fb7e5a2d3de4b0c8be94c22eda
silvio009 <rm550821@fiap.com.br>
2023-06-28 14:53:28 -0300

    finalizando

--- formulario JavaScript/JS/camera.js
+++ formulario JavaScript/JS/camera.js
@@ -4,6 +4,9 @@ const video = document.querySelector("[data-video]");
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
 const mensagem = document.querySelector("[data-mensagem]")
+const botaoEnviarFoto = document.querySelector("[data-enviar]")
+
+const imagemURL = "";
 
 botaoIniciarCamera.addEventListener("click",async function(){
   const iniciarVideo = await navigator.mediaDevices.
@@ -22,4 +25,15 @@ botaoTirarFoto.addEventListener("click", function() {
 
     campoCamera.style.display = "none";
     mensagem.style.display = "block";
+})
+
+botaoEnviarFoto.addEventListener('click', () => {
+    const receberDadosExistentes = localStorage.getItem("cadastro");
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno))
+
+    window.location.href = '../pages/abrir-conta-form-3.html';
 })
